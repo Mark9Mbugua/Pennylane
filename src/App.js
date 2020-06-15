621f6ba7cb18c1a40b056f8192a20ae9e2e7d007
Markize <mbuguamark@gmail.com>
2020-06-15 13:28:57 +0300

    Created header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header/Header.js';
-import Apartments from './components/Apartments/Apartments.js';
-import ContactUs from './components/ContactUs/ContactUs.js';
-import Location from './components/Location/Location.js';
-import Ammenities from './components/Ammenities/Ammenities.js';
-import Footer from './components/Footer/Footer.js';
+import Header from './components/Header/Header';
+import Apartments from './components/Apartments/Apartments';
+import ContactUs from './components/ContactUs/ContactUs';
+import Location from './components/Location/Location';
+import Ammenities from './components/Ammenities/Ammenities';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
