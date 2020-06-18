4e34a4de533128b4c5c4bd0a39940c8c5a9d60da
Markize <mbuguamark@gmail.com>
2020-06-18 18:44:10 +0300

    created apartments section

--- src/components/Apartments/Apartments.js
+++ src/components/Apartments/Apartments.js
@@ -1,11 +1,62 @@
 import React from 'react';
+import oneBedroom from '../../images/1bdsitting.jpg';
+import twoBedroom from '../../images/2bdexterior.jpg';
+import { Link } from 'react-router-dom';
+import { 
+    ApartmentsContainer,
+    ApartmentsSection,
+    FeaturedApartment,
+    FeaturedImg,
+    SectionTitle,
+    TitleContainer,
+    Line,
+    Info,
+    ImgContainer,
+    PriceTop,
+    PriceTitle
+ } from './apartments.styles';
 
 const Apartments = () => {
     return (
-        <div>
-            Apartments Section
-        </div>
-    )
+        <ApartmentsSection>
+            <TitleContainer>
+                <SectionTitle>Apartments</SectionTitle>
+                <Line />
+            </TitleContainer>
+            <ApartmentsContainer>
+                <FeaturedApartment>
+                    <ImgContainer>
+                        <FeaturedImg src={oneBedroom} alt='One Bedroom' />
+                        <PriceTop>
+                            <PriceTitle>Ksh 20,000</PriceTitle>
+                            <p>per month</p>
+                        </PriceTop>
+                    </ImgContainer>
+                    <Info>One bedroom</Info>
+                </FeaturedApartment>
+                <FeaturedApartment>
+                    <ImgContainer>  
+                        <FeaturedImg src={twoBedroom} alt='Two Bedroom' />
+                        <PriceTop>
+                            <PriceTitle>Ksh 35,000</PriceTitle>
+                            <p>per month</p>
+                        </PriceTop>
+                    </ImgContainer>
+                    <Info>Two bedroom Penthouse</Info>
+                </FeaturedApartment>
+                <FeaturedApartment>
+                    <ImgContainer>
+                        <FeaturedImg src={oneBedroom} alt='Three Bedroom' />
+                        <PriceTop>
+                            <PriceTitle>Ksh 30,000</PriceTitle>
+                            <p>per month</p>
+                        </PriceTop>
+                    </ImgContainer>
+                    <Info>Three bedroom</Info>
+                </FeaturedApartment>
+            </ApartmentsContainer>
+        </ApartmentsSection>
+    );
 }
 
 export default Apartments
