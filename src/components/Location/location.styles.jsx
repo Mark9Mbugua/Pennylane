ae840b926912a271776a7eb9f7445ad1e5f5553f
Mark9Mbugua <mbuguamark@gmail.com>
2021-04-08 23:42:49 +0300

    Made location section responsive on all major screen sizes

--- src/components/Location/location.styles.jsx
+++ src/components/Location/location.styles.jsx
@@ -1,31 +1,89 @@
 import styled from 'styled-components';
 import { ApartmentsContainer, ApartmentsSection } from '../Apartments/apartments.styles';
 
-export const LocationSection = styled(ApartmentsSection)`
+export const LocationSection = styled.section`
+    min-height: 100vh;
+    display: grid;
     grid-template-rows: 18% 1fr;
-    
-    @media screen and (max-width: 1024px) {
-        grid-template-rows: 20% 1fr;
-        min-height: 70vh;
+
+    @media only screen 
+    and (min-width: 1440px) 
+    and (max-width: 1920px) {
+        min-height: 90vh;
     }
 
-    @media screen and (max-width:  480px) {
+    @media only screen 
+    and (min-width: 1280px) 
+    and (max-width: 1280px) 
+    and (min-height: 1024px) 
+    and (max-height: 1024px){
+        min-height: 85vh;
+    }
+
+    @media only screen 
+    and (min-width: 1024px) 
+    and (max-height: 1366px)
+    and (-webkit-min-device-pixel-ratio: 1.5) {
+        min-height: 80vh;
+    }
+
+    @media screen and (max-width: 480px) {
         min-height: 90vh;
+        grid-template-rows: 25% 1fr;
     }
 `;
 
 export const LocationContainer = styled(ApartmentsContainer)`
     grid-template-columns: 1fr;
     padding: 10px 20px;
+    width: 100%;
+    align-content: center;
 
-    @media screen and (max-width: 1024px) {
+    @media screen and (max-width: 1920px) {
         grid-template-columns: 1fr;
         padding: 20px 20px;
         width: 100%;
     }
 
-    @media screen and (max-width:  480px) {
+    @media only screen 
+    and (min-width: 1440px) 
+    and (max-width: 1920px) {
+        min-height: 60%;
+    }
+
+    @media only screen 
+    and (min-width: 1280px) 
+    and (max-width: 1280px) 
+    and (min-height: 1024px) 
+    and (max-height: 1024px){
+        min-height: 65%;
+    }
+
+    @media only screen 
+    and (min-width: 1024px) 
+    and (max-height: 1366px)
+    and (orientation: landscape) 
+    and (-webkit-min-device-pixel-ratio: 1.5) {
+        padding: 10px 30px;
+    }
+
+    @media only screen 
+    and (min-width: 1024px) 
+    and (max-height: 1366px)
+    and (orientation: portrait) 
+    and (-webkit-min-device-pixel-ratio: 1.5) {
+        height: 80%;
+    }
+
+    @media screen 
+    and (min-width: 480px)
+    and (max-width: 768px) {
+        height: 80%;
+    }
+
+    @media screen and (max-width: 480px) {
         padding: 10px;
+        height: 85%;
     }
 `;
 
@@ -41,10 +99,4 @@ export const MapCanvas = styled.div`
     background: none !important;
     height: 100%;
     width: 100%;
-
-    @media screen and (max-width: 1024px) {
-        iframe {
-            height: 100%;
-        }
-    }
 `;
